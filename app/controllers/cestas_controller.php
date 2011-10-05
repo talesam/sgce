287126ed19cc8a880a70d140acf72bb1f433018c
Edinei L. Cipriani <phpedinei@gmail.com>
2011-10-05 02:29:51 -0300

    gerar cestas

--- app/controllers/cestas_controller.php
+++ app/controllers/cestas_controller.php
@@ -20,118 +20,57 @@ class CestasController extends AppController {
 	
 	
 	public function admin_gerar_cestas(){
-		
-			/* Instancio a classe ESTOQUE para ser usado durante a execucao deste algoritimo */
-			$estoque =& ClassRegistry::init('Estoque');
-			
-			/* Instancio a classe DEFINICOESCESTA para ser usado durante a execucao deste algoritimo */
-			$definicoescesta =& ClassRegistry::init('Definicoescesta');
-			
-			/* Listo todas as definicoes da cesta para saber o q é necessario para montar a mesma */
-			$definicoescestas = $definicoescesta->find('all');
-			
-			/* Inicio a variavel que será responsável para exibir ao usuario quantas cestas foi possível gerar */
-			$totalCestas = 0;
-			
-			/* Inicio o ok=true para que o sistema entre em loop pelo menos uma vez */
-			$ok = true;
+		$defCesta = ClassRegistry::init('Definicoescesta');
+		$estoqueCesta = ClassRegistry::init('Estoque');
+		$estoqueCesta->recursive = -1;
+		$cestas = 0;
+		$ok = true;
+		while(true){
+			/* Pego todas as definições da cesta  */
+			foreach($defCesta->find('all') as $def){
 
-			while($ok){
-				$cesta = array();
-				foreach($definicoescestas as $m){
-					/* 
-					* Na linha abaixo o sistema procura a quantidade de estoque e se ela é suficiente para compor a cesta 
-					* caso contrario não sera possível cadastrar a cesta
-					* */
-					$t = $estoque->find('first', array('fields' => 'SUM(Estoque.quantidade) as `total`' ,'conditions' => array('Estoque.definicoescesta_id' => $m['Definicoescesta']['id'])));
-					
-					if($t[0]['total'] >= $m['Definicoescesta']['quantidade']){
-						
-						/*
-						* Listo os estoques cuja data não esteja  e a quantidade seja > 0  em ordem de vencimento
-						* os itens com prazo menor para vencimento serão composto na cesta com prioridade
-						* */
-						$estoques_para_cesta = $estoque->find('all', array(
-							'order' => array('Estoque.data_vencimento' => 'asc'),
-							'conditions' => array(
-								'Estoque.definicoescesta_id' => $m['Definicoescesta']['id'], 
-								'Estoque.quantidade >' => 0) 
+				/* Verifico se possúi itens de definicao da cesta no estoque ordenando pela data de vencimento */
+				$estoque = $estoqueCesta->find('first', 
+					array(
+						'conditions' => 
+							array(
+								'Estoque.definicoescesta_id' => $def['Definicoescesta']['id'], 
+								'Estoque.quantidade >' => 0,
+								'Estoque.complemento_qt ' => $def['Definicoescesta']['quantidade']
+							), 
+							'order' => 
+								array(
+									'Estoque.data_vencimento' => 'ASC'
+								)
 							)
 						);
-						
-						/* Loop do vetor */
-						foreach($estoques_para_cesta as $estoque_para_cesta){
-							
-							/* 
-							* Se a quantidade de complemento de cesta for igual a definição da cesta
-							* Ex.: 5 Kilos de arroz, e ter no estoque 1 pacote de arroz de 5kg
-							*  */
-							if($estoque_para_cesta['Estoque']['complemento_qt'] == $m['Definicoescesta']['quantidade']){
-								$cesta['Estoque'][] = array('id' => $estoque_para_cesta['Estoque']['id']);
-								$data[] = array(
-									'id' =>  $estoque_para_cesta['Estoque']['id'], 
-									'quantidade' => $estoque_para_cesta['Estoque']['quantidade'] -1
-								);
-							}
-							/* 
-							* Se a quantidade de complemento de cesta for maior, e em pacotes diferentes
-							* Ex.: 5 Kilos de arroz, e ter no estoque 5 pacotes de 1kg de arroz
-							*  */
-							elseif($estoque_para_cesta['Estoque']['complemento_qt'] > $m['Definicoescesta']['quantidade']){
-								$cesta['Estoque'][] = array('id' => $estoque_para_cesta['Estoque']['id']);
-								$data[] = array(
-									'id' =>  $estoque_para_cesta['Estoque']['id'], 
-									'quantidade' => $estoque_para_cesta['Estoque']['quantidade'] - $m['Definicoescesta']['quantidade']
-								);
-							}else{
-								
-								/* Logica para estoque quebrado
-								* Ex.: na definição da cesta precisa de 5Kg de arroz, e no estoque existem mais de um cadastro:
-								* 2 Pacotes de 1kg
-								* 3 Pacotes de 2kg
-								* O sistema precisa entender que vai ter q tirar do estoque 1 pacote de 1kg e  2 de 2kg para compor a cesta
-								*  */
-								
 
-							}
-						}
-					}else{ 
-					    /* Não é possível cadastrar cesta pois não tem estoque suficiente */
-						$ok = false;
-					}
-				}
-				
-				/* Se ok=true significa que foi possível cadastrar uma cesta */
-				if($ok){
-					
-					/* Data que foi gerada a cesta */
-					$cesta['Cesta']['data_gerada'] = date('Y-m-d H:i:s');				
-					/* Salva a cesta no banco */
-					if($this->Cesta->saveAll($cesta)){
-						/* Atualiza o estoque 
-						*  de modo que foi tirado itens para compor a cesta.
-						* */
-						foreach($data as $d){
-							$estoque->save(array('id' => $d['id'], 'quantidade' => $d['quantidade']));
-						}
-						
-					}
-					
-					/* Aqui eu incremento a cesta */
-					$totalCestas++;
-				}
-				
-				/* Limpo da memória, para que uma nova cesta possa ser gerada */
-				unset($data);
+				if(!empty($estoque)){
+					pr($estoque); 
+					$remover[] = array('id' => $estoque['Estoque']['id'], 'quantidade' => ($estoque['Estoque']['quantidade'] - 1) );
+				}else{ $ok = false; }
+			}
+			
+			if($ok != true){
+				break;
+			}
+			$cestas++;
+		}
+		
+		if($cestas > 0){
+			for($i=0; $i < $cestas; $i++){
+				$this->Cesta->create();
+				$this->Cesta->save(array('data_gerada' => date('Y-m-d H:i:s')));
+			}
+			$this->Session->setFlash($cestas . ' cestas geradas.', 'flash_success');
+		}else{
+			$this->Session->setFlash('Não foi possível gerar nenhuma cesta.', 'flash_error');
+		}
+		
+		$this->redirect('index');
 
-			}
-			if($totalCestas > 0){
-				$this->Session->setFlash($totalCestas .' nova(s) cesta(s) gerada(s)', 'flash_success');
-			}else{
-				$this->Session->setFlash('Nenhuma cesta gerada.', 'flash_error');
-			}
-			$this->redirect($this->referer());
+		
+		
 	}
-	
 }
 ?>
